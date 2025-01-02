e0f7f9263d0b3dd246cb4b4904d8ed98ce55ddec
Adhil-Akbar <356adhil@gmail.com>
2025-01-02 12:58:16 +0530

    Update service worker version and resource hashes in Flutter TV build

--- public/flutter-builds/tv/flutter_bootstrap.js
+++ public/flutter-builds/tv/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2995538673"
+    serviceWorkerVersion: "1338005890"
   }
 });
